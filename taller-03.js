e6a0f66bdd4952fe55aba15a7bfdf63d96cb2eab
t-santoya <dejesussantiago7912@gmail.com>
2025-09-04 09:29:38 -0500

    Taller 3

---
+++ taller-03.js
@@ -0,0 +1,97 @@
+// taller-03.js
+
+// Punto 1
+function desglosarString(texto, tipo) {
+    let vocales = "aeiouAEIOU"
+    let contador = 0
+
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i]
+        if (vocales.indexOf(letra) !== -1) {
+            if (tipo === "vocales") {
+                contador = contador + 1
+            }
+        } else {
+            if (tipo === "consonantes") {
+                contador = contador + 1
+            }
+        }
+    }
+
+    return contador
+}
+
+console.log(desglosarString("murcielagos", "vocales"))
+console.log(desglosarString("murcielagos", "consonantes")) 
+
+
+// Punto 2
+function twoSum(lista, objetivo) {
+    for (let i = 0; i < lista.length; i++) {
+        for (let j = i + 1; j < lista.length; j++) {
+            if (lista[i] + lista[j] === objetivo) {
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)) 
+console.log(twoSum([3, 4, 2], 6)) 
+
+
+// Punto 3
+function conversionRomana(romano) {
+    let valores = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let total = 0
+    for (let i = 0; i < romano.length; i++) {
+        let actual = valores[romano[i]]
+        let siguiente = valores[romano[i + 1]]
+
+        if (siguiente && actual < siguiente) {
+            total = total - actual
+        } else {
+            total = total + actual
+        }
+    }
+    return total
+}
+
+console.log(conversionRomana("III")) 
+console.log(conversionRomana("XIV")) 
+console.log(conversionRomana("MMXXIV")) 
+console.log(conversionRomana("MCMXCVII")) 
+
+
+// Punto 4
+function descomposicion(cadena) {
+    let partes = cadena.split(",")
+    let palabra = partes[0]
+    let diccionario = []
+
+    for (let i = 1; i < partes.length; i++) {
+        diccionario[i - 1] = partes[i]
+    }
+
+    for (let i = 0; i < diccionario.length; i++) {
+        for (let j = 0; j < diccionario.length; j++) {
+            if (diccionario[i] + diccionario[j] === palabra) {
+                return [diccionario[i], diccionario[j]]
+            }
+        }
+    }
+
+    return []
+}
+
+console.log(descomposicion("malhumor,al,hum,humor,m,mal,malhu")) // ["mal", "humor"]
