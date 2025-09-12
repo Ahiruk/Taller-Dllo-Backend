b738a008d3ed9b5c3c0cd72d8c007a967706bd0d
ahiruk <santoyal@uninorte.edu.co>
2025-09-12 11:03:14 -0500

    no habia subido esto

--- Practica_PP.js
+++ Practica_PP.js
@@ -10,3 +10,18 @@ function CantStrings(int){
 }
 let arrayStrings = ["Hola", "Mi nombre es Martin Elias y estoy cursando Programacion para profesionales", "Estoy aprendiendo mucho", "JavaScript es un lenguaje muy divertido"];
 console.log(CantStrings(arrayStrings));
+
+//PUNTO 2
+
+function exponencial(base, exponente){
+    let acum = 1;
+    for (let i = 0; i < exponente; i++) {
+        acum = acum * base;
+    }
+    return acum;
+}
+
+console.log(exponencial(2,3));
+
+
+//PUNTO 3
